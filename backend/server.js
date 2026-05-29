5d8a38a247f800b5169df9b6ab2a12ae48079849
TharunR18 <its.tharun018@gmail.com>
2026-05-29 19:35:16 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ db();
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }))
 
 app.get("/", (req, res) => {
     res.send("Server is running");
